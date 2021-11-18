8f77b0e24c51c0027e5540c2892cf6de0b35eb2c
celestinacoded <90314150+celestinacoded@users.noreply.github.com>
2021-11-18 12:29:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./Components/Header/Header";
 
-
-const App = () => {
-  return (
-    <Router>
-    <Header/>
-    <Routes> 
-      <Route />
-    </Routes>
-    </Router>
-
-    
-  );
-}
-=======
 import React from "react";
 import SamHomePage from "./Components/SamSalHomePage/samHomePage";
 import HomePage from "./Components/TaibatHomePage/HomePage";
@@ -33,6 +15,5 @@ const App = () => {
     </div>
   );
 };
->>>>>>> aef7934d8eb88aaa8557dc8fe999030d018bf437
 
 export default App;
